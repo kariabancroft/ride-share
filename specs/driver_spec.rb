f8fed9b77cf68a23f3c46a75bb7a65159c693c85
Kari Bancroft <kariabancroft@gmail.com>
2017-03-02 17:12:47 -0800

    Setup spec files with spec helper and rake file to run tests

---
+++ specs/driver_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'spec_helper'
+
+describe "Driver" do
+  describe "Driver#initialize" do
+    it "Takes an ID, name and vehicle identification num" do
+      
+    end
+  end
+end
---
+++ specs/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+# require 'CSV'
+require_relative '../lib/driver'
+require_relative '../lib/rider'
+require_relative '../lib/trip'
